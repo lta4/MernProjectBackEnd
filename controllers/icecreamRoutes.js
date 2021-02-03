6c8cdf58178fe47801dc9a4df76dda3dbb9df8c7
Andrew <andrew.mk4@yahoo.com>
2021-02-03 07:25:49 -0800

    routes open

--- controllers/icecreamRoutes.js
+++ controllers/icecreamRoutes.js
@@ -33,64 +33,64 @@ router.get("/", (req, res) => {
 });
 
 // GET-by first name
-// router.get("/:id", (req, res) => {
-//     Icecream.findById(req.params.id)
-//         .then((icecream) =>
-//             res.json({
-//                 status: 200,
-//                 icecream: icecream,
-//             })
-//         ).catch((err) => res.json({
-//             status: 400,
-//             err: err,
-//         }));
-// });
+router.get("/:id", (req, res) => {
+    Icecream.findById(req.params.id)
+        .then((icecream) =>
+            res.json({
+                status: 200,
+                icecream: icecream,
+            })
+        ).catch((err) => res.json({
+            status: 400,
+            err: err,
+        }));
+});
 
 // GET-by flavor
-// router.get("/:id", (req, res) => {
-//     Icecream.findById(req.params.id)
-//         .then((icecream) => 
-//             res.json({
-//                 status: 200,
-//                 icecream: icecream,
-//             })
-//         ).catch((err) => res.json({
-//             status: 400,
-//             err: err,
-//         }));
-// });
+router.get("/:id", (req, res) => {
+    Icecream.findById(req.params.id)
+        .then((icecream) => 
+            res.json({
+                status: 200,
+                icecream: icecream,
+            })
+        ).catch((err) => res.json({
+            status: 400,
+            err: err,
+        }));
+});
 
 // PUT -by name and flavor
-// router.put("/:id", (req, res) => {
-//     Icecream.findByIdAndUpdate(req.params.id, req.body, { new: true })
-//         .then((icecream) =>
-//             res.json({
-//                 status: 200,
-//                 icecream: icecream,
-//             })
-//         ).catch((err) => res.json({
-//             status: 400,
-//             err: err,
-//         }));
-// });
+router.put("/:id", (req, res) => {
+    Icecream.findByIdAndUpdate(req.params.id, req.body, { new: true })
+        .then((icecream) =>
+            res.json({
+                status: 200,
+                icecream: icecream,
+            })
+        ).catch((err) => res.json({
+            status: 400,
+            err: err,
+        }));
+});
 
 // CREATE
-// router.post("/", async (req, res) => {
-//     res.json(await Icecream.create(req.body));
-// });
+router.post("/", async (req, res) => {
+    res.json(await Icecream.create(req.body));
+});
 
 // DELETE- by id
-// router.delete("/:id", (req, res) => {
-//     Icecream.findByIdAndDelete(req.params.id)
-//         .then((icecream) =>
-//             res.json({
-//                 status: 200,
-//                 msg: "item deleted",
-//             })
-//         ).catch((err) => res.json({
-//             status: 400,
-//             err: err,
-//         }));
-// });
+router.delete("/:id", (req, res) => {
+    Icecream.findByIdAndDelete(req.params.id)
+        .then((icecream) =>
+            res.json({
+                status: 200,
+                msg: "item deleted",
+            })
+        ).catch((err) => res.json({
+            status: 400,
+            err: err,
+        }));
+});
 
 module.exports = router
